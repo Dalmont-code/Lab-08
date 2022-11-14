4dd11e535bca59b19e2106d489414fab4e723df6
Lorenzo Dalmonte <lorenzo.dalmonte4@studio.unibo.it>
2022-11-14 12:47:44 +0100

    Implementata la classe SimpleGUI esercizio 82

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,15 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.WindowConstants;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +17,32 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private final JFrame frame = new JFrame("My First Java Graphical Interface");
 
+    public SimpleGUI() {
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        final JTextArea text = new JTextArea();
+        final JButton button = new JButton("Save");
+        panel.add(text, BorderLayout.CENTER);
+        panel.add(button, BorderLayout.SOUTH);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                final Controller controller = new Controller();
+                try {
+                    controller.add(text.getText());
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+        frame.add(panel);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new SimpleGUI();
+    }
 }
