d347ec3bc20f6973af46f1124f928bca2f084e63
Lorenzo Dalmonte <lorenzo.dalmonte4@studio.unibo.it>
2022-11-14 12:20:13 +0100

    Implementata la classe Controller esercizio 82

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,42 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
 
+    private static final String DEFAULT_PATH = System.getProperty("user.home")
+            + System.getProperty("file.separator")
+            + "output.txt";
+    private File file;
+    
+    public Controller(final String pathname) {
+        file = new File(pathname);
+    }
+
+    public Controller() {
+        this(DEFAULT_PATH);
+    }
+    
+    public void setFile(final String pathname) {
+        this.file = new File(pathname);
+    }
+
+    public File getFile() {
+        return this.file;
+    }
+
+    public String getPath() {
+        return this.file.getPath();
+    }
+
+    public void add(final String arg) throws IOException {
+        try (final PrintStream ps = new PrintStream(this.file)) {
+            ps.print(arg);
+        }
+    }
 }
