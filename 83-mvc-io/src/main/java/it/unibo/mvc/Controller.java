da6080b9ba8d7104574485258d4400d093495277
Lorenzo Dalmonte <lorenzo.dalmonte4@studio.unibo.it>
2022-11-15 11:11:52 +0100

    Implementato SimpleController e scritto javadoc per interfaccia Controller

--- 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,43 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
- *
+ * This is an interface for a controller
  */
 public interface Controller {
+
+    /**
+     * This method sets the current message.
+     * It throws a NullPointerException if the parameter is null.
+     * 
+     * @param msg
+     * @throws NullPointerException
+     */
+    public void setString(String msg) throws NullPointerException;
+
+    /**
+     * This method returns the current message.
+     * If the message is unset, it returns an empty string.
+     * 
+     * @return the current message
+     */
+    public String getString();
+
+    /**
+     * This method returns the list of messages printed up to this point.
+     * It returns an empty list if nothing was printed.
+     * 
+     * @return the list of printed messages
+     */
+    public List<String> getHistory();
+
+    /**
+     * This method prints the current method.
+     * If the message is unset it throws an IllegalStateException.
+     * 
+     * @throws IllegalStateException
+     */
+    public void print() throws IllegalStateException;
 
 }
--- 83-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,40 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
 
+    private String msg;
+    
+    @Override
+    public void setString(final String msg) {
+        if (msg == null) {
+            throw new NullPointerException("Can't set message to null");
+        } else {
+            this.msg = msg;
+        }
+    }
+
+    @Override
+    public String getString() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<String> getHistory() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void print() {
+        // TODO Auto-generated method stub
+        
+    }
+    
 }
